8bc27f168f6c792f46b47bad3abc176c2497295c
maoridi <maoridi16@gmail.com>
2025-07-31 12:19:00 +0300

    remove test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-def get_absolute_path(relative):
-    current_path = Path(__file__).parent
-    return (current_path / relative).resolve()
-
-
-config_path = get_absolute_path('../scripts/init_vm.sh')
-
-print(config_path)
